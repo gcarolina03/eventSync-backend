f23a037316f2ed19b79bb610e850ea83bd518912
G. Carolina Hernandez <carolinahrndez3@gmail.com>
2023-07-14 11:06:31 +0100

    add update state of request and price of the event

--- api/controllers/request.controller.js
+++ api/controllers/request.controller.js
@@ -29,15 +29,13 @@ const createRequest  = async (req, res) => {
       event.eventRequests.push(request._id)
       await event.save()
 
-      res.status(200).json({ request })
+      return res.status(200).json({ request })
     }
   } catch (error) {
-    console.error(error)
-    res.status(500).json({ message: 'Failed to save the request' })
+    return res.status(500).json({ message: 'Failed to save the request' })
   }
 }
 
-// Create a new review
 const getAllUserRequests  = async (req, res) => {
   try {
     if(!req.body.userId) { req.body.userId = res.locals.user.id }
@@ -62,11 +60,50 @@ const getAllUserRequests  = async (req, res) => {
                       })
                       .sort({ date_of_request: -1 })
                       .exec();
-    return  res.status(200).json({ request })
+    return res.status(200).json({ request })
   } catch (error) {
-    res.status(500).json({ message: 'Failed to get requests' })
+    return res.status(500).json({ message: 'Failed to get requests' })
+  }
+}
+
+const updateRequestState = async (req, res) => {
+  try {
+    const request = await Request
+      .findById(req.params.id)
+      .populate({
+        path: 'serviceId',
+        populate: {
+          path: 'categoryId'
+        }
+      })
+    
+    if (!request) return res.status(404).json({ error: 'Request not found' })
+    
+    //check if user log is owner of the service in the request
+    if(res.locals.user.id !== request.serviceId.userId.toString()) return res.status(500).json('Unauthorized')
+    
+    const event = await Event.findById(request.eventId)
+    if (!event) return res.status(404).json({ error: 'Event not found' })
+    
+    let dataEvent = {}
+    console.log(request.serviceId.categoryId)
+    if (request.serviceId.categoryId.title == 'Location' && req.body.state == 'confirmed') {
+      dataEvent = { total_price: event.total_price + request.serviceId.price, img_url: request.serviceId.img_url }
+      console.log(dataEvent)
+    } else if (req.body.state == 'confirmed') {
+      dataEvent = { total_price: event.total_price + request.serviceId.price }
+      console.log(dataEvent)
+    }
+    console.log(req.body)
+
+    const updatedEvent = await Event.updateOne({ _id: event._id }, dataEvent);
+    console.log(updatedEvent);    
+    await Request.updateOne({ _id: request._id }, {state: req.body.state})
+    return res.status(200).json({ request })
+  } catch (error) {
+    return res.status(500).json({ message: 'Failed to update requests' })
   }
 }
 
 
-module.exports = { createRequest, getAllUserRequests }
+module.exports = { createRequest, getAllUserRequests, updateRequestState }
--- api/routes/request.router.js
+++ api/routes/request.router.js
@@ -1,9 +1,11 @@
 const router = require('express').Router()
-const { createRequest, getAllUserRequests } = require('../controllers/request.controller')
+const { createRequest, getAllUserRequests, updateRequestState } = require('../controllers/request.controller')
 const { checkAuth } = require('../../middlewares/auth') 
 
 router.post('/', checkAuth, createRequest)
 
 router.get('/', checkAuth, getAllUserRequests)
+
+router.put('/:id', checkAuth, updateRequestState)
 
 module.exports = router
