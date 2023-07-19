9a72f26659a1f08c71e20aa9d7695d56321466ca
G. Carolina Hernandez <carolinahrndez3@gmail.com>
2023-07-19 09:54:14 +0100

    deleted city controller, router, model

--- api/routes/city.router.js
+++
@@ -1,8 +0,0 @@
-const router = require('express').Router()
-const { createCity, getCities } = require('../controllers/city.controller')
-const { checkAuth, checkAdmin } = require('../../middlewares/auth') 
-
-router.post('/', checkAuth, checkAdmin, createCity)
-router.get('/', checkAuth, getCities)
-
-module.exports = router
