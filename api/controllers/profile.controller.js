0595329f4b0f7c426045e2013af84d36497a3340
Carolina Hernandez <carolinahrndez3@gmail.com>
2025-02-17 22:24:45 +0000

    agregar funcionalidad para editar el perfil de usuario y actualizar la imagen

--- api/controllers/profile.controller.js
+++ api/controllers/profile.controller.js
@@ -1,3 +1,9 @@
+const bcrypt = require("bcrypt");
+const User = require("../models/user.model");
+const cloudinary = require("../../db/cloudinary");
+const { default: mongoose } = require("mongoose");
+const jwt = require("jsonwebtoken");
+
 const showProfile = async (req, res) => {
   try {
     return res.status(200).json({
@@ -13,4 +19,79 @@ const showProfile = async (req, res) => {
   }
 };
 
-module.exports = { showProfile };
+const editProfile = async (req, res) => {
+  const session = await mongoose.startSession();
+
+  try {
+    session.startTransaction();
+
+		console.log(req.body)
+    const userId = res.locals.user._id;
+
+    if (!userId) {
+      await session.abortTransaction();
+      session.endSession();
+
+      return res.status(400).json({
+        success: false,
+        message: "User ID is required",
+      });
+    }
+
+		console.log(userId)
+
+    const user = await User.findById(userId).session(session);
+    if (!user) {
+      await session.abortTransaction();
+      session.endSession();
+
+      return res.status(404).json({
+        success: false,
+        message: "User not found",
+      });
+    }
+
+		console.log(user);
+
+    if (req.first_name && req.first_name != '') {
+      user.first_name = req.first_name;
+    }
+
+    if (req.last_name && req.last_name != "") {
+      console.log("hola");
+      user.last_name = req.last_name;
+    }
+
+    if (req.file) {
+      const result = await cloudinary.uploader.upload(req.file.path);
+      user.img_url = result.secure_url;
+    }
+
+    if (req.body.password && req.body.password != "") {
+      const hashedPassword = bcrypt.hashSync(password, 10);
+			user.password = hashedPassword;
+    }
+
+		await user.save({ session });
+
+		await session.commitTransaction();
+		session.endSession();
+
+    return res.status(200).json({
+      success: true,
+      user,
+			token,
+    });
+  } catch (err) {
+		await session.abortTransaction();
+		session.endSession();
+
+    return res.status(500).json({
+      success: false,
+      error: err.message,
+      message: "Error updating user profile",
+    });
+  }
+};
+
+module.exports = { showProfile, editProfile };
--- api/routes/profile.router.js
+++ api/routes/profile.router.js
@@ -1,9 +1,10 @@
 const router = require('express').Router()
 const { checkAuth } = require('../../middlewares/auth') 
-const { showProfile } = require('../controllers/profile.controller')
+const { showProfile, editProfile } = require('../controllers/profile.controller')
 const myServicesRouter = require('./myservice.router')
 
 router.get('/', checkAuth, showProfile)
+router.put("/", checkAuth, editProfile);
 router.use('/services', myServicesRouter)
 
 module.exports = router
