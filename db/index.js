28444f334aabc27ef8ef819b181607e4d56ebaf6
G. Carolina Hernández <carolinahrndez3@gmail.com>
2025-03-01 18:33:01 +0000

    add dbname

--- db/index.js
+++ db/index.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose')
-const url = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}`
+const url = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}/${process.env.DB_NAME}?retryWrites=true&w=majority`
 
 async function connectToDatabase() {
   try {
